2eaa159aa88dc9ee1ee82e35984ec2131825789f
tnsdo <jungeun2115@gmail.com>
2024-08-25 21:51:08 +0900

    wa..

--- src/home-bsignup.tsx
+++ src/home-bsignup.tsx
@@ -1,22 +1,24 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import homecat from "./assets/homecat.png";
+import wacat from "./assets/wacat.png";
 
 const SignContainer = styled.div`
   width: 500px;
   height: 300px;
   background-color: #f4f4f4;
   border-radius: 15px;
-  left: 50%;
   transform: translateX(10%);
   margin-top: 150px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  position: relative;
 `;
 const Welcome = styled.div`
   color: black;
@@ -25,12 +27,35 @@ const Welcome = styled.div`
   font-family: "Rubik Bubbles", system-ui;
 `;
 
+const CatWrapper = styled.div`
+  position: relative;
+  width: 100px;
+  height: 100px;
+  margin-bottom: 20px;
+`;
+
 const HomeCat = styled.img`
   border-radius: 10%;
   width: 100px;
   height: 100px;
   object-fit: fill;
   margin-bottom: 20px;
+  cursor: pointer;
+  z-index: 1;
+  position: relative;
+`;
+
+const WaCat = styled.img`
+  border-radius: 10%;
+  width: 100px;
+  height: 100px;
+  object-fit: fill;
+  margin-bottom: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  opacity: 0;
 `;
 
 const Sign = styled.div`
@@ -56,6 +81,20 @@ const SignUp = styled.button`
 
 function HomeBsignup() {
   const navigate = useNavigate();
+  const [isWaCatVisible, setWaCatVisible] = useState(false);
+
+  const handleHomeCatClick = () => {
+    setWaCatVisible(true);
+  };
+
+  useEffect(() => {
+    if (isWaCatVisible) {
+      const timer = setTimeout(() => {
+        setWaCatVisible(false);
+      }, 200); // Show wacat for 200ms
+      return () => clearTimeout(timer);
+    }
+  }, [isWaCatVisible]);
 
   const handSignInClick = () => {
     navigate("/signin");
@@ -66,7 +105,10 @@ function HomeBsignup() {
   return (
     <SignContainer>
       <Welcome>Welcome!</Welcome>
-      <HomeCat src={homecat} alt="homecat" />
+      <CatWrapper>
+        {isWaCatVisible && <WaCat src={wacat} alt="wacat" />}
+        <HomeCat src={homecat} alt="homecat" onClick={handleHomeCatClick} />
+      </CatWrapper>
       <Sign>
         <SignIn onClick={handSignInClick}>Sign in</SignIn>
         <SignUp onClick={handSignUpClick}>Sign up</SignUp>
